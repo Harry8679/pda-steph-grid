6525d7208dcbfbf6da4d61292fcd9fe83bc9b6b9
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-05 23:59:55 +0200

    Props added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import ProfileCard from './ProfileCard';
 function App() {
   return (
     <div className="App">
-      <ProfileCard />
+      <ProfileCard title="Alexa" handle="@alexa99" />
+      <ProfileCard title="Cortana" handle="@cortana32" />
+      <ProfileCard title="Siri" handle="@siri01" />
     </div>
   );
 }
