94cbc5bff996c8e99c949268dc2c2c5cade94bc5
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-06 09:07:32 +0200

    Items showed

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,18 @@
 const ProfileCard = ({ title, handle, image }) => {
   return (
-    <div>
-        <img src={image} alt={image} />
-        <div>Title is {title}</div>
-        <div>Handle is {handle}</div>
+    <div className='card'>
+        <div className="card-image">
+            <figure className='image is-1by1'>
+                <img src={image} alt={image} />
+            </figure>
+        </div>
+
+        <div className="card-content">
+            <div className="media-content">
+                <div className='title is-4'>Title is {title}</div>
+                <div className='subtitle is-6'>Handle is {handle}</div>
+            </div>
+        </div>
     </div>
   )
 }
