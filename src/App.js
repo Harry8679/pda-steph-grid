0afe992f16ac1b370ba83a5f56179329b39bc473
Harry EMARH <emarh.harry.code@gmail.com>
2025-08-06 08:25:04 +0200

    Style removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <ProfileCard title="Alexa" handle="@alexa99" />
       <ProfileCard title="Cortana" handle="@cortana32" />
       <ProfileCard title="Siri" handle="@siri01" />
